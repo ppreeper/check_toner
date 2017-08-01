690e981aaec0676b805e1372b009c0f3285b895d
Peter Preeper <ppreeper@gmail.com>
2017-08-01 10:07:36 -0600

    update tests for tonerLevel and tonerOutput

--- check_toner.go
+++ check_toner.go
@@ -42,27 +42,32 @@ var utax = &toners{
 	".1.3.6.1.2.1.43.11.1.1.8.1.4", ".1.3.6.1.2.1.43.11.1.1.9.1.4",
 }
 
-func tonerOutput(color string, maxValue string, lvlValue string) {
+func tonerOutput(color string, maxValue string, lvlValue string) string {
+	color = strings.ToUpper(color)
 	max, errm := strconv.Atoi(maxValue)
 	lvl, errl := strconv.Atoi(lvlValue)
-	// var output string
+	var output string
+
 	if errm == nil && errl == nil {
 		if max != 0 {
 			level := 100 * float64(float64(lvl)/float64(max))
+			tLevels := "-- Toner Levels: " + lvlValue + " of " + maxValue + " Remaining | "
+			tLevels += strconv.FormatFloat(level, 'f', 0, 64) + "\n"
 			if level >= 10.0 {
-				fmt.Printf("%s Toner OK -- Toner Levels: %s of %s Remaining | %s\n", color, lvlValue, maxValue, lvlValue)
+				output = color + " Toner OK " + tLevels
 			} else {
-				fmt.Printf("%s Toner LOW -- Toner Levels: %s of %s Remaining | %s\n", color, lvlValue, maxValue, lvlValue)
+				output = color + " Toner LOW " + tLevels
 			}
 		}
 	}
-	return
+	return output
 }
 
-func tonerLevel(color string, brand string) {
+func tonerLevel(color string, brand string) string {
 	color = strings.ToUpper(color)
 	var tonerColor string
 	var t toners
+	var output string
 
 	switch brand {
 	case "HP":
@@ -73,25 +78,23 @@ func tonerLevel(color string, brand string) {
 		t = *utax
 	}
 
-	fmt.Printf("%v", t.cyanMax)
-
 	if color == "C" {
 		tonerColor = "CYAN"
-		tonerOutput(tonerColor, getSNMPValue(t.cyanMax), getSNMPValue(t.cyanLvl))
+		output = tonerOutput(tonerColor, getSNMPValue(t.cyanMax), getSNMPValue(t.cyanLvl))
 	}
 	if color == "M" {
 		tonerColor = "MAGENTA"
-		tonerOutput(tonerColor, getSNMPValue(t.magentaMax), getSNMPValue(t.magentaLvl))
+		output = tonerOutput(tonerColor, getSNMPValue(t.magentaMax), getSNMPValue(t.magentaLvl))
 	}
 	if color == "Y" {
 		tonerColor = "YELLOW"
-		tonerOutput(tonerColor, getSNMPValue(t.yellowMax), getSNMPValue(t.yellowLvl))
+		output = tonerOutput(tonerColor, getSNMPValue(t.yellowMax), getSNMPValue(t.yellowLvl))
 	}
 	if color == "K" {
 		tonerColor = "BLACK"
-		tonerOutput(tonerColor, getSNMPValue(t.kromaMax), getSNMPValue(t.kromaLvl))
+		output = tonerOutput(tonerColor, getSNMPValue(t.kromaMax), getSNMPValue(t.kromaLvl))
 	}
-	return
+	return output
 }
 
 func getSNMPValue(oid string) string {
@@ -109,19 +112,15 @@ func getSNMPValue(oid string) string {
 	return fmt.Sprintf("%s", g.ToBigInt(result.Variables[0].Value))
 }
 
+// main function
 func main() {
-	// All the interesting pieces are with the variables declared above, but
-	// to enable the flag package to see the flags defined there, one must
-	// execute, typically at the start of main (not init!):
 	flag.Parse()
-	// We don't run it here because this is not a main function and
-	// the testing suite has already parsed the flags.
-	//var flg = ""
 	if *host == "" {
 		fmt.Println("Host not set")
 	} else {
 		//fmt.Printf("Host:\t%s\nColor:\t%s\nBrand:\t%s\n", *host, *color, *brand)
-		tonerLevel(*color, *brand)
+		r := tonerLevel(*color, *brand)
+		fmt.Printf("%v", r)
 	}
 	return
 }
--- check_toner_test.go
+++ check_toner_test.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 // mulColorTests use values that you know are right
 var mulColorTests = []struct {
@@ -11,18 +9,89 @@ var mulColorTests = []struct {
 	lvlValue string
 	expected string
 }{
-	{"1d6", "d", "", ""},
+	{"cyan", "100", "85", "CYAN Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"cyan", "10000", "8500", "CYAN Toner OK -- Toner Levels: 8500 of 10000 Remaining | 85\n"},
+	{"cyan", "10000", "900", "CYAN Toner LOW -- Toner Levels: 900 of 10000 Remaining | 9\n"},
+	{"cyan", "100", "9", "CYAN Toner LOW -- Toner Levels: 9 of 100 Remaining | 9\n"},
+	{"MAGENTA", "100", "85", "MAGENTA Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"MAGENTA", "10000", "8500", "MAGENTA Toner OK -- Toner Levels: 8500 of 10000 Remaining | 85\n"},
+	{"MAGENTA", "10000", "900", "MAGENTA Toner LOW -- Toner Levels: 900 of 10000 Remaining | 9\n"},
+	{"MAGENTA", "100", "9", "MAGENTA Toner LOW -- Toner Levels: 9 of 100 Remaining | 9\n"},
+	{"YELLOW", "100", "85", "YELLOW Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"YELLOW", "10000", "8500", "YELLOW Toner OK -- Toner Levels: 8500 of 10000 Remaining | 85\n"},
+	{"YELLOW", "10000", "900", "YELLOW Toner LOW -- Toner Levels: 900 of 10000 Remaining | 9\n"},
+	{"YELLOW", "100", "9", "YELLOW Toner LOW -- Toner Levels: 9 of 100 Remaining | 9\n"},
+	{"BLACK", "100", "85", "BLACK Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"BLACK", "10000", "8500", "BLACK Toner OK -- Toner Levels: 8500 of 10000 Remaining | 85\n"},
+	{"BLACK", "10000", "900", "BLACK Toner LOW -- Toner Levels: 900 of 10000 Remaining | 9\n"},
+	{"BLACK", "100", "9", "BLACK Toner LOW -- Toner Levels: 9 of 100 Remaining | 9\n"},
 }
 
 // TestTonerOutput test
 func TestTonerOutput(t *testing.T) {
 	for _, mt := range mulColorTests {
-		tonerOutput("K", "100", "90")
-		t.Errorf("%v", mt.color)
+		r := tonerOutput(mt.color, mt.maxValue, mt.lvlValue)
+		if r != mt.expected {
+			t.Errorf("%s %s", mt.color, r)
+		}
 	}
 }
 
 // BenchmarkPattern
 func BenchmarkTonerOutput(b *testing.B) {
-	// run the Fib function b.N times
+	for n := 0; n < b.N; n++ {
+		tonerOutput("CYAN", "900", "10000")
+	}
+}
+
+// mulColorTests use values that you know are right
+var mulColorBrand = []struct {
+	color    string
+	brand    string
+	expected string
+}{
+	{"C", "HP", "CYAN Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"C", "UTAX", "CYAN Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"C", "KYOCERA", "CYAN Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"M", "HP", "MAGENTA Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"M", "UTAX", "MAGENTA Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"M", "KYOCERA", "MAGENTA Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"Y", "HP", "YELLOW Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"Y", "UTAX", "YELLOW Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"Y", "KYOCERA", "YELLOW Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"K", "HP", "BLACK Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"K", "UTAX", "BLACK Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+	{"K", "KYOCERA", "BLACK Toner OK -- Toner Levels: 85 of 100 Remaining | 85\n"},
+}
+
+// TestTonerLevel test
+func TestTonerLevel(t *testing.T) {
+	*host = "192.168.101.80"
+	for _, mt := range mulColorBrand {
+		r := tonerLevel(mt.color, mt.brand)
+		if r != mt.expected {
+			t.Errorf("%s %s", mt.color, r)
+		}
+	}
+	*host = "192.168.101.52"
+	for _, mt := range mulColorBrand {
+		r := tonerLevel(mt.color, mt.brand)
+		if r != mt.expected {
+			t.Errorf("%s %s", mt.color, r)
+		}
+	}
+	*host = "192.168.101.28"
+	for _, mt := range mulColorBrand {
+		r := tonerLevel(mt.color, mt.brand)
+		if r != mt.expected {
+			t.Errorf("%s %s", mt.color, r)
+		}
+	}
+}
+
+// BenchmarkTonerLevel
+func BenchmarkTonerLevel(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		tonerOutput("CYAN", "900", "10000")
+	}
 }
